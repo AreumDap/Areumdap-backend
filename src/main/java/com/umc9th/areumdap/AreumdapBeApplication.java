b36b8cbf0a2a0cc9d2cfd074d3c134abddcbbb58
pooreumjung <pooreumjung02@naver.com>
2026-01-04 15:58:18 +0900

    [Chore] 초기 세팅

---
+++ src/main/java/com/umc9th/areumdap/AreumdapBeApplication.java
@@ -0,0 +1,13 @@
+package com.umc9th.areumdap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AreumdapBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AreumdapBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/umc9th/areumdap/AreumdapBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc9th.areumdap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AreumdapBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
