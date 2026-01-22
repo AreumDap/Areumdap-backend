7c0dc442ceeda18844e7fe1bff230e8b80d97c4f
pooreumjung <pooreumjung02@naver.com>
2026-01-22 17:56:29 +0900

    [Deploy] #33 Firebase 배포 설정

--- src/main/java/com/umc9th/areumdap/common/config/FirebaseConfig.java
+++ src/main/java/com/umc9th/areumdap/common/config/FirebaseConfig.java
@@ -4,23 +4,22 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.FileInputStream;
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.credentials.path}")
+    private String firebaseCredentialsPath;
+
     @PostConstruct
     public void init() {
         try {
-            InputStream serviceAccount =
-                getClass().getClassLoader()
-                    .getResourceAsStream("firebase/firebase-service-account.json");
-
-            if(serviceAccount == null) {
-                throw new Exception("firebase-service-account.json not found");
-            }
+            InputStream serviceAccount = new FileInputStream(firebaseCredentialsPath);
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
