51227f497c58307a7160e6b33ddfb03e08d13cec
Crom <doyoung.yang2005@gmail.com>
2026-01-24 05:02:06 +0900

    [Feat] #31 : 리포지토리 생성 - QuestionBankRepository   - 문제은행에서 랜덤으로 count만큼 미션을 뽑는 쿼리 작성 - UserQuestionQueryRepository   - 당일에 랜덤 배정된 질문인지 확인하는 쿼리 작성

---
+++ src/main/java/com/umc9th/areumdap/domain/question/repository/QuestionBankRepository.java
@@ -0,0 +1,15 @@
+package com.umc9th.areumdap.domain.question.repository;
+
+import com.umc9th.areumdap.domain.question.entity.QuestionBank;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface QuestionBankRepository extends JpaRepository<QuestionBank, Long> { //문제은행에서 랜덤으로 count만큼 미션을 뽑는 쿼리
+    @Query(value = "SELECT * FROM question_bank WHERE id NOT IN(" +
+            "SELECT question_bank_id FROM user_question WHERE user_id = :userId" +
+            ") ORDER BY RANDOM() LIMIT :count", nativeQuery = true)
+    List<QuestionBank> findRandomQuestions(@Param("userId") Long userId, @Param("count") int count);
+}
---
+++ src/main/java/com/umc9th/areumdap/domain/question/repository/UserQuestionQueryRepository.java
@@ -0,0 +1,14 @@
+package com.umc9th.areumdap.domain.question.repository;
+
+import com.umc9th.areumdap.domain.question.entity.UserQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface UserQuestionQueryRepository extends JpaRepository<UserQuestion, Long> { //당일에 랜덤 배정된 질문인지 확인하는 쿼리
+    @Query(value = "SELECT * FROM user_question WHERE user_id = :userId AND created_at BETWEEN :start AND :end ORDER BY created_at DESC LIMIT 5", nativeQuery = true)
+    List<UserQuestion> findByUserIdAndCreatedAtBetween(@Param("userId") Long userId, @Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+}
