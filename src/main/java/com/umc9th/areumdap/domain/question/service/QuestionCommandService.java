0181e6a0aa28a3794a43bb87d8cc24cec0abad3a
Crom <doyoung.yang2005@gmail.com>
2026-01-24 05:06:15 +0900

    [Feat] #31 : 질문 조회 / 질문 할당 서비스 로직 작성 - 당일 날짜로 질문 조회해서 생성된 것이 5개 미만이면 새로 질문을 할당. - 5개 이상인 경우 이미 할당된 것으로 판단하여 기존 할당된 질문으로 응답. - 만약 더이상 할당할 질문이 Question Bank 테이블에 없으면 에러 응답

---
+++ src/main/java/com/umc9th/areumdap/domain/question/service/QuestionCommandService.java
@@ -0,0 +1,48 @@
+package com.umc9th.areumdap.domain.question.service;
+
+import com.umc9th.areumdap.common.exception.GeneralException;
+import com.umc9th.areumdap.common.status.ErrorStatus;
+import com.umc9th.areumdap.domain.question.entity.QuestionBank;
+import com.umc9th.areumdap.domain.question.entity.UserQuestion;
+import com.umc9th.areumdap.domain.question.repository.QuestionBankRepository;
+import com.umc9th.areumdap.domain.question.repository.UserQuestionRepository;
+import com.umc9th.areumdap.domain.user.entity.User;
+import com.umc9th.areumdap.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class QuestionCommandService {
+
+    private final QuestionBankRepository questionBankRepository;
+    private final UserQuestionRepository userQuestionRepository;
+    private final UserRepository userRepository;
+
+    public List<UserQuestion> assignRandomQuestions(Long userId){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new GeneralException(ErrorStatus.USER_NOT_FOUND));
+
+        List<QuestionBank> randomQuestions = questionBankRepository.findRandomQuestions(userId, 5);
+
+        if (randomQuestions.size() < 5) {
+            throw new GeneralException(ErrorStatus.QUESTION_BANK_NOT_ENOUGH);
+        }
+
+        List<UserQuestion> userQuestions = randomQuestions.stream()
+                .map(q -> UserQuestion.builder()
+                        .user(user)
+                        .questionBank(q)
+                        .content(q.getContent())
+                        .tag(q.getTag())
+                        .isUsed(false)
+                        .build())
+                .toList();
+        return userQuestionRepository.saveAll(userQuestions);
+    }
+}
---
+++ src/main/java/com/umc9th/areumdap/domain/question/service/QuestionQueryService.java
@@ -0,0 +1,28 @@
+package com.umc9th.areumdap.domain.question.service;
+
+import com.umc9th.areumdap.domain.question.entity.UserQuestion;
+import com.umc9th.areumdap.domain.question.repository.UserQuestionQueryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class QuestionQueryService {
+
+    private final UserQuestionQueryRepository userQuestionQueryRepository;
+
+    public List<UserQuestion> getTodayQuestions(Long userId){ //오늘배정된 질문 조회
+        LocalDateTime start = LocalDate.now().atStartOfDay();
+        LocalDateTime end = LocalDate.now().atTime(LocalTime.MAX);
+
+        return userQuestionQueryRepository.findByUserIdAndCreatedAtBetween(userId, start, end);
+    }
+
+}
