efdb970765b3b410fc7be10a5232bafb476117cf
Crom <doyoung.yang2005@gmail.com>
2026-01-25 02:52:14 +0900

    [Chore] #31: 리포지토리 인자 개행 구조 변경 && 리포지토리 반환값 수정

--- src/main/java/com/umc9th/areumdap/domain/question/repository/QuestionBankRepository.java
+++ src/main/java/com/umc9th/areumdap/domain/question/repository/QuestionBankRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface QuestionBankRepository extends JpaRepository<QuestionBank, Long> { //문제은행에서 랜덤으로 count만큼 미션을 뽑는 쿼리
     @Query(value = "SELECT * FROM question_bank WHERE id NOT IN(" +
             "SELECT question_bank_id FROM user_question WHERE user_id = :userId" +
             ") ORDER BY RANDOM() LIMIT :count", nativeQuery = true)
-    List<QuestionBank> findRandomQuestions(@Param("userId") Long userId, @Param("count") int count);
+    Optional<List<QuestionBank>> findRandomQuestions(@Param("userId") Long userId,
+                                                     @Param("count") int count
+    );
 }
