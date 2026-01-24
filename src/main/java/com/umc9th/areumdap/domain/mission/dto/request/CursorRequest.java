08ae3175e0b6ddaf09be052691d02f8b1de82b5e
kimjihoon <jihungim500@gmail.com>
2026-01-25 00:47:11 +0900

    [Delete] #26 불필요한 import 삭제

--- src/main/java/com/umc9th/areumdap/domain/mission/dto/request/CursorRequest.java
+++ src/main/java/com/umc9th/areumdap/domain/mission/dto/request/CursorRequest.java
@@ -2,8 +2,6 @@ package com.umc9th.areumdap.domain.mission.dto.request;
 
 import com.umc9th.areumdap.domain.mission.enums.Tag;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Pattern;
-import org.apache.catalina.valves.rewrite.InternalRewriteMap;
 
 import java.time.OffsetDateTime;
 
