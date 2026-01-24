2422e8958325c61c51c63e31846387280aed1ab3
Crom <doyoung.yang2005@gmail.com>
2026-01-25 02:55:38 +0900

    [Refactor] #31: UserQuestion 엔티티 User도메인으로 리팩토링

--- src/main/java/com/umc9th/areumdap/domain/question/service/QuestionQueryService.java
+++ src/main/java/com/umc9th/areumdap/domain/question/service/QuestionQueryService.java
@@ -1,6 +1,6 @@
 package com.umc9th.areumdap.domain.question.service;
 
-import com.umc9th.areumdap.domain.question.entity.UserQuestion;
+import com.umc9th.areumdap.domain.user.entity.UserQuestion;
 import com.umc9th.areumdap.domain.question.repository.UserQuestionQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
