fc968ffd5ad653909e28066605185f4659579bee
Crom <doyoung.yang2005@gmail.com>
2026-01-25 02:52:14 +0900

    [Chore] #31: 리포지토리 인자 개행 구조 변경 && 리포지토리 반환값 수정

--- src/main/java/com/umc9th/areumdap/domain/question/repository/UserQuestionQueryRepository.java
+++ src/main/java/com/umc9th/areumdap/domain/question/repository/UserQuestionQueryRepository.java
@@ -7,8 +7,12 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface UserQuestionQueryRepository extends JpaRepository<UserQuestion, Long> { //당일에 랜덤 배정된 질문인지 확인하는 쿼리
     @Query(value = "SELECT * FROM user_question WHERE user_id = :userId AND created_at BETWEEN :start AND :end ORDER BY created_at DESC LIMIT 5", nativeQuery = true)
-    List<UserQuestion> findByUserIdAndCreatedAtBetween(@Param("userId") Long userId, @Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+    Optional<List<UserQuestion>> findByUserIdAndCreatedAtBetween(@Param("userId") Long userId,
+                                                                @Param("start") LocalDateTime start,
+                                                                @Param("end") LocalDateTime end
+    );
 }
