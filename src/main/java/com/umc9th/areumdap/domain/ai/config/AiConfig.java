23a0c9fe7b254573c1cd11251acabbfe5f2d5107
Function <67085909+Function1790@users.noreply.github.com>
2026-01-25 18:00:13 +0900

    [Feat] #45 AI 환경 세팅

---
+++ src/main/java/com/umc9th/areumdap/domain/ai/config/AiConfig.java
@@ -0,0 +1,8 @@
+package com.umc9th.areumdap.domain.ai.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AiConfig {
+
+}
---
+++ src/main/java/com/umc9th/areumdap/domain/ai/service/AiService.java
@@ -0,0 +1,11 @@
+package com.umc9th.areumdap.domain.ai.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AiService {
+
+}
