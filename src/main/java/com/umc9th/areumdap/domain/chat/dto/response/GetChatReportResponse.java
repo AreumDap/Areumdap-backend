56454a20281e78c36da2a99f4bb396a3ff8998d9
jihoonkim501 <jihungim500@gmail.com>
2026-01-27 18:13:52 +0900

    [Feat] #55 레포트 Response 생성

---
+++ src/main/java/com/umc9th/areumdap/domain/chat/dto/response/GetChatReportResponse.java
@@ -0,0 +1,20 @@
+package com.umc9th.areumdap.domain.chat.dto.response;
+
+import com.umc9th.areumdap.domain.mission.dto.response.MissionSummaryResponse;
+import com.umc9th.areumdap.domain.report.dto.response.ReportInsightResponse;
+import com.umc9th.areumdap.domain.report.dto.response.ReportTagResponse;
+
+import java.util.List;
+
+public record GetChatReportResponse(
+        String title,
+        Integer messageCount,
+        String depth,
+        Integer durationMinutes,
+        String summaryContent,
+        List<ReportTagResponse> reportTags ,
+        List<ReportInsightResponse> insightContents,
+        List<MissionSummaryResponse> missions
+
+) {
+}
