6d2099ed7911e1178fad748490223d50e9af90b6
jihoonkim501 <jihungim500@gmail.com>
2026-01-27 18:11:29 +0900

    [Feat] #55 레포트 조회 Controller & Docs 생성

--- src/main/java/com/umc9th/areumdap/domain/chat/controller/ChatController.java
+++ src/main/java/com/umc9th/areumdap/domain/chat/controller/ChatController.java
@@ -3,32 +3,33 @@ package com.umc9th.areumdap.domain.chat.controller;
 import com.umc9th.areumdap.common.response.ApiResponse;
 import com.umc9th.areumdap.common.status.SuccessStatus;
 import com.umc9th.areumdap.domain.chat.controller.docs.ChatControllerDocs;
-import com.umc9th.areumdap.domain.chat.dto.request.CreateChatThreadRequest;
-import com.umc9th.areumdap.domain.chat.dto.response.CreateChatThreadResponse;
-import com.umc9th.areumdap.domain.chat.service.ChatCommandService;
-import jakarta.validation.Valid;
+import com.umc9th.areumdap.domain.chat.dto.response.GetChatReportResponse;
+import com.umc9th.areumdap.domain.chat.service.ChatQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/chatbot")
+@RequestMapping("/api/chat")
 @RequiredArgsConstructor
 public class ChatController implements ChatControllerDocs {
-    private final ChatCommandService chatCommandService;
 
-    @PostMapping("/start")
-    public ResponseEntity<ApiResponse<CreateChatThreadResponse>> createChatThread(
+    private final ChatQueryService chatQueryService;
+
+    @Override
+    @GetMapping("/reports/{reportId}")
+    public ResponseEntity<ApiResponse<GetChatReportResponse>> getChatReport(
             @AuthenticationPrincipal Long userId,
-            @Valid @RequestBody CreateChatThreadRequest request
+            @PathVariable Long reportId
     ) {
+
         return ApiResponse.success(
-                SuccessStatus.CREATE_CHAT_THREAD_SUCCESS,
-                chatCommandService.createChatThread(userId, request)
+                SuccessStatus.GET_CHAT_REPORT_SUCCESS,
+                chatQueryService.getChatReport(userId, reportId)
         );
     }
 
