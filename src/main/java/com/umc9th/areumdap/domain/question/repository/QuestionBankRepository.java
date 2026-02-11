67082a18644dd9c735dd9e882c5765659d301509
Crom <doyoung.yang2005@gmail.com>
2026-02-11 22:35:51 +0900

    [Fix] #131 자식 질문 조회되지 않도록 수정

--- src/main/java/com/umc9th/areumdap/domain/question/repository/QuestionBankRepository.java
+++ src/main/java/com/umc9th/areumdap/domain/question/repository/QuestionBankRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public interface QuestionBankRepository extends JpaRepository<QuestionBank, Long> { //문제은행에서 랜덤으로 pageable변수만큼 미션을 뽑는 쿼리
     @Query("SELECT qb FROM QuestionBank qb " +
-           "WHERE qb.id NOT IN (SELECT uq.questionBank.id FROM UserQuestion uq WHERE uq.user.id = :userId) " +
+           "WHERE qb.id NOT IN (SELECT uq.questionBank.id FROM UserQuestion uq WHERE uq.user.id = :userId AND uq.parentQuestion IS NULL) " +
            "AND qb.id != 91 " +
            "ORDER BY function('random')")
     List<QuestionBank> findRandomQuestions(
